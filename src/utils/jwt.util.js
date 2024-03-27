679d2ff9ffbc3b9b5c539cd64c5ae36f32e35912
Ali Kumbul <ali.kumbul@iotiq.net>
2024-03-27 21:56:04 +0300

    jwt util for signing and verifying

---
+++ src/utils/jwt.util.js
@@ -0,0 +1,20 @@
+const jwt = require('jsonwebtoken'),
+    config = require('../config/environment.variables.config');
+
+module.exports.signJwt = async (payload) => {
+    const token = jwt.sign(payload,
+        config.jwtSecret,
+        { expiresIn: "1h" })
+
+    return token;
+};
+
+module.exports.getPayloadFromReq = (req) => {
+    const token = req.headers["authorization"];
+    const bearerToken = token.slice(7);
+    return jwt.verify(bearerToken, config.jwtSecret);
+};
+
+module.exports.getPayloadFromToken = (bearerToken) => {
+    return jwt.verify(bearerToken, config.jwtSecret);
+};
