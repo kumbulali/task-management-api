f882bd23b3c72e1d08ec9e3ae74d73f53fcfdbd1
Ali Kumbul <ali.kumbul@iotiq.net>
2024-03-27 21:58:08 +0300

    user model

---
+++ src/models/user.model.js
@@ -0,0 +1,45 @@
+const mongoose = require('mongoose'),
+    crypto = require('crypto');
+
+const UserSchema = new mongoose.Schema({
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    salt: {
+        type: String
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+UserSchema.pre('save', function (next) {
+    if (this.password) {
+        this.salt = crypto.randomBytes(16).toString('base64');
+        this.password = this.hashPassword(this.password);
+    }
+    next();
+});
+
+UserSchema.methods.hashPassword = function (password) {
+    if (this.salt && password) {
+        return crypto.pbkdf2Sync(password, this.salt, 10000, 64, 'sha512').toString('base64');
+    }
+    else
+        return password;
+};
+
+UserSchema.methods.authenticate = function (password) {
+    return this.password === this.hashPassword(password);
+};
+
+const User = mongoose.model('User', UserSchema);
+
+module.exports = User;
