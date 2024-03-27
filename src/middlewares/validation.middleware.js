f8d095df28964f947a4c621c345bdef0035536ec
Ali Kumbul <ali.kumbul@iotiq.net>
2024-03-27 21:55:15 +0300

    task creation validation

--- src/middlewares/validation.middleware.js
+++ src/middlewares/validation.middleware.js
@@ -1,4 +1,5 @@
 const Joi = require('joi');
+Joi.objectId = require('joi-objectid')(Joi);
 
 module.exports.validateRegister = function (req, res, next) {
     const schema = Joi.object({
@@ -16,6 +17,18 @@ module.exports.validateLogin = function (req, res, next) {
     const schema = Joi.object({
         email: Joi.string().required().max(50).label('Email'),
         password: Joi.string().min(6).required().label('Password')
+    });
+
+    const { error } = schema.validate(req.body);
+    next(error);
+};
+
+module.exports.validateCreateTask = function (req, res, next) {
+    const schema = Joi.object({
+        title: Joi.string().required().max(50).label('Title'),
+        description: Joi.string().label('Description'),
+        completed: Joi.boolean().label('Completed'),
+        assignee: Joi.objectId()
     });
 
     const { error } = schema.validate(req.body);
