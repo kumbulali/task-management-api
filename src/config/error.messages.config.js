013ee24d706500dca666f38097a8a3b39be2a386
Ali Kumbul <ali.kumbul@iotiq.net>
2024-03-28 16:25:00 +0300

    new error messages

--- src/config/error.messages.config.js
+++ src/config/error.messages.config.js
@@ -1,7 +1,10 @@
 const CustomError = require("../utils/custom.error");
 
 const errorMessages = {
-    invalid_credentials: new CustomError("Invalid Credentials", "invalid-credentials", 401)
+    invalid_credentials: new CustomError("Invalid Credentials", "invalid-credentials", 401),
+    user_not_found: new CustomError("User not found", "user-not-found", 400),
+    task_not_found: new CustomError("Task not found", "task-not-found", 400),
+    validation_error: new CustomError("Please check request body and parameters. It is not valid.", "validation-error", 400)
 }
 
 module.exports = errorMessages;
