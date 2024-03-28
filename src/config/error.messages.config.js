55e6e558958241e097a2834db6b5430b8a22c0d5
Ali Kumbul <ali.kumbul@iotiq.net>
2024-03-29 01:09:28 +0300

    error messages refactoring

--- src/config/error.messages.config.js
+++ src/config/error.messages.config.js
@@ -1,9 +1,9 @@
 const CustomError = require("../utils/custom.error");
 
 const errorMessages = {
-    invalid_credentials: new CustomError("Invalid Credentials", "invalid-credentials", 401),
-    user_not_found: new CustomError("User not found", "user-not-found", 400),
-    task_not_found: new CustomError("Task not found", "task-not-found", 400),
+    invalid_credentials: new CustomError("Invalid credentials.", "invalid-credentials", 401),
+    user_not_found: new CustomError("User not found.", "user-not-found", 400),
+    task_not_found: new CustomError("Task not found or you do not have privilege to access.", "task-not-found", 400),
     validation_error: new CustomError("Please check request body and parameters. It is not valid.", "validation-error", 400)
 }
 
