9452f4822243f592ac22ebb9c784f0961c79740d
Ali Kumbul <ali.kumbul@iotiq.net>
2024-03-29 02:22:47 +0300

    validate update task and get task id

--- src/middlewares/validation.middleware.js
+++ src/middlewares/validation.middleware.js
@@ -1,5 +1,11 @@
-const Joi = require('joi');
-Joi.objectId = require('joi-objectid')(Joi);
+const mongoose = require('mongoose'),
+    Joi = require('joi'),
+    ObjectID = mongoose.Types.ObjectId;
+
+const JoiObjectId = Joi.string().custom((value, helpers) => {
+    const filtered = ObjectID.isValid(value)
+    return !filtered ? helpers.error("any.invalid") : value;
+}, "invalid objectId",);
 
 module.exports.validateRegister = function (req, res, next) {
     const schema = Joi.object({
@@ -28,9 +34,35 @@ module.exports.validateCreateTask = function (req, res, next) {
         title: Joi.string().required().max(50).label('Title'),
         description: Joi.string().label('Description'),
         completed: Joi.boolean().label('Completed'),
-        assignee: Joi.objectId()
+        assignee: JoiObjectId
     });
 
     const { error } = schema.validate(req.body);
     next(error);
+};
+
+module.exports.validateTaskID = function (req, res, next) {
+    const schema = Joi.object({
+        taskId: JoiObjectId
+    });
+
+    const { error } = schema.validate(req.params);
+    next(error);
+};
+
+module.exports.validateUpdateTask = function (req, res, next) {
+    const paramsSchema = Joi.object({
+        taskId: JoiObjectId
+    });
+
+    const fieldsToUpdateSchema = Joi.object({
+        title: Joi.string().max(50).label('Title'),
+        description: Joi.string().label('Description'),
+        completed: Joi.boolean().label('Completed'),
+        assignee: JoiObjectId
+    });
+
+    const paramsError = paramsSchema.validate(req.params).error;
+    const bodyError = fieldsToUpdateSchema.validate(req.body).error;
+    next(paramsError || bodyError);
 };
