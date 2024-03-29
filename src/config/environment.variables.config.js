903944ee677aa09da08420eac7b0403bfd4e9e7e
Ali Kumbul <ali.kumbul@iotiq.net>
2024-03-29 14:41:13 +0300

    session and google client secret injection to envvar

--- src/config/environment.variables.config.js
+++ src/config/environment.variables.config.js
@@ -4,12 +4,15 @@ dotenv.config();
 
 const SERVER = {
     hostname: process.env.HOSTNAME || "localhost",
-    port: process.env.PORT || 3000
+    port: process.env.PORT || 3000,
+    googleClientId: process.env.GOOGLE_CLIENT_ID,
+    googleClientSecret: process.env.GOOGLE_CLIENT_SECRET
 };
 
 const config = {
     server: SERVER,
-    jwtSecret: process.env.JWT_SECRET
+    jwtSecret: process.env.JWT_SECRET,
+    sessionSecret: process.env.SESSION_SECRET
 };
 
 module.exports = config;
