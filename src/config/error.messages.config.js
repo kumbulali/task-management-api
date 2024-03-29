d782b0de2e7becbb8020d5a3d16f7aa89d9fd87b
Ali Kumbul <ali.kumbul@iotiq.net>
2024-03-29 14:41:20 +0300

    new error messages

--- src/config/error.messages.config.js
+++ src/config/error.messages.config.js
@@ -4,7 +4,9 @@ const errorMessages = {
     invalid_credentials: new CustomError("Invalid credentials.", "invalid-credentials", 401),
     user_not_found: new CustomError("User not found.", "user-not-found", 400),
     task_not_found: new CustomError("Task not found or you do not have privilege to access.", "task-not-found", 400),
-    validation_error: new CustomError("Please check request body and parameters. It is not valid.", "validation-error", 400)
+    validation_error: new CustomError("Please check request body and parameters. It is not valid.", "validation-error", 400),
+    user_not_have_email: new CustomError("User does not have email", "user-not-have-email", 400),
+    google_oauth_error: new CustomError("Google OAuth 2.0 error.", "google-oauth-error", 500)
 }
 
 module.exports = errorMessages;
